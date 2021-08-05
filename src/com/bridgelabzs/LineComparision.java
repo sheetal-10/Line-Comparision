f21dea9589fb57fba9cd24e7c37137b850ecb336
sheetal-10 <sheetalp257@gmail.com>
2021-08-05 20:45:49 +0530

    Added: Welcome message

---
+++ src/com/bridgelabzs/LineComparision.java
@@ -0,0 +1,7 @@
+package com.bridgelabzs;
+
+public class LineComparision {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Computation program");
+	}
+}
